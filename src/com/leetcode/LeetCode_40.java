0a415e53a516d096fdf7faf31f97df923319f8ac
shihengli2 <529737202@qq.com>
2017-07-04 09:58:59 +0800

    39,40 题

--- src/com/leetcode/LeetCode_40.java
+++ src/com/leetcode/LeetCode_40.java
@@ -5,24 +5,7 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
-/**
- * Given a set of candidate numbers (C) (without duplicates) and a target number
- * (T), find all unique combinations in C where the candidate numbers sums to T.
- * 
- * The same repeated number may be chosen from C unlimited number of times.
- * 
- * Note: All numbers (including target) will be positive integers. The solution
- * set must not contain duplicate combinations. 
- * 
- * For example, given candidate set
- * [2, 3, 6, 7] and target 7, A solution set is: 
- * [ 
- * 	 [7], 
- *   [2, 2, 3] 
- * ]
- *
- */
-public class LeetCode_39 {
+public class LeetCode_40 {
 
 	public List<List<Integer>> combinationSum(int[] candidates, int target) {
 		 List<List<Integer>> ret = new LinkedList<List<Integer>>();  
@@ -45,10 +28,9 @@ public class LeetCode_39 {
 			 if(newTarget>=0){
 				 List<Integer> copy = new ArrayList<Integer>(list);  
 	                copy.add(candidates[i]);  
-	                recurse(copy, newTarget, candidates, i , ret);  
+	                recurse(copy, newTarget, candidates, i + 1, ret);  
 	            } else  
 	                break; 
 		 }
 	 }
-	
 }
