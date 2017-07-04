03efb9efc0b1462491051414b9da27ce5787f7b4
shihengli2 <529737202@qq.com>
2017-07-04 10:05:29 +0800

    39 merge

--- src/com/leetcode/LeetCode_39.java
+++ src/com/leetcode/LeetCode_39.java
@@ -12,43 +12,45 @@ import java.util.List;
  * The same repeated number may be chosen from C unlimited number of times.
  * 
  * Note: All numbers (including target) will be positive integers. The solution
- * set must not contain duplicate combinations. 
+ * set must not contain duplicate combinations.
  * 
- * For example, given candidate set
- * [2, 3, 6, 7] and target 7, A solution set is: 
- * [ 
- * 	 [7], 
- *   [2, 2, 3] 
- * ]
+ * For example, given candidate set [2, 3, 6, 7] and target 7, A solution set
+ * is: [ [7], [2, 2, 3] ]
  *
  */
 public class LeetCode_39 {
 
-	public List<List<Integer>> combinationSum(int[] candidates, int target) {
-		 List<List<Integer>> ret = new LinkedList<List<Integer>>();  
-	        Arrays.sort(candidates);  
-	        recurse(new ArrayList<Integer>(), target, candidates, 0, ret);  
-	        return ret;  
+	public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+		List<List<Integer>> ret = new LinkedList<List<Integer>>();
+		Arrays.sort(candidates);
+		rescue(new ArrayList<Integer>(), target, candidates, 0, ret);
+		return ret;
 	}
-	
-	
-	 private void recurse(List<Integer> list, int target, int[] candidates, int index, List<List<Integer>> ret) {  
-		 	
-		 if(target == 0 ){
-			  if(!ret.contains(list))  
-	                ret.add(list);  
-	            return; 
-		 }
-		 
-		 for(int i = index ; i<candidates.length ; i++){
-			 int newTarget = target - candidates[i] ;
-			 if(newTarget>=0){
-				 List<Integer> copy = new ArrayList<Integer>(list);  
-	                copy.add(candidates[i]);  
-	                recurse(copy, newTarget, candidates, i , ret);  
-	            } else  
-	                break; 
-		 }
-	 }
-	
+
+	private static void rescue(List<Integer> list, int target, int[] candidates, int index, List<List<Integer>> ret) {
+		if (target == 0) {
+			ret.add(list);
+			return;
+		}
+
+		for (int i = index; i < candidates.length; i++) {
+			int newTarget = target - candidates[i];
+			if (newTarget >= 0) {
+				List<Integer> copy = new ArrayList<Integer>(list);
+				copy.add(candidates[i]);
+				rescue(copy, newTarget, candidates, i, ret);
+			} else {
+				break;
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		int[] candidates = {2};
+		int target = 8 ;
+		 List<List<Integer>>  result = combinationSum(candidates, target);
+		 System.out.println(result);
+	}
 }
