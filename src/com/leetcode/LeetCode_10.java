67b41c2939ff08b5fa7b9e771fcd6cb0d20bfe50
shl <hengshi@cisco.com>
2018-04-07 22:35:56 +0800

    测试git

--- src/com/leetcode/LeetCode_10.java
+++ src/com/leetcode/LeetCode_10.java
@@ -1,7 +1,7 @@
 package com.leetcode;
 /*
 '.' Matches any single character.
-'*' Matches zero or more of the preceding element.     *代表的是字符可以出现0或者任意多次 注意可能是0次
+'*' Matches zero or more of the preceding element.     *浠ｈ〃鐨勬槸瀛楃鍙互鍑虹幇0鎴栬�呬换鎰忓娆� 娉ㄦ剰鍙兘鏄�0娆�
 
 The matching should cover the entire input string (not partial).
 
@@ -9,16 +9,17 @@ The function prototype should be:
 bool isMatch(const char *s, const char *p)
 
 Some examples:
-isMatch("aa","a") → false
-isMatch("aa","aa") → true
-isMatch("aaa","aa") → false
-isMatch("aa", "a*") → true
-isMatch("aa", ".*") → true
-isMatch("ab", ".*") → true
-isMatch("aab", "c*a*b") → true
+isMatch("aa","a") 鈫� false
+isMatch("aa","aa") 鈫� true
+isMatch("aaa","aa") 鈫� false
+isMatch("aa", "a*") 鈫� true
+isMatch("aa", ".*") 鈫� true
+isMatch("ab", ".*") 鈫� true
+isMatch("aab", "c*a*b") 鈫� true
 
 */
 
+//错了哦
 public class LeetCode_10 {
 
 	public boolean isMatch(String s, String p) {
